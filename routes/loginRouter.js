ba35aeae6a41c5d96b4989e0764fcf23f58ce38f
kongtaemin <rhdxoals@gmail.com>
2021-11-11 19:29:28 +0900

    post요청추가

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
-router.post("/chat", function (req, res, next) {
+router.post("/login", function (req, res, next) {
   res.send("post요청성공했어임마");
 });
 
