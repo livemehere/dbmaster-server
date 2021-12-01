0fac0362b0274588aca92b2a8dfa46925de91512
kongtaemin <rhdxoals@gmail.com>
2021-12-01 23:23:31 +0900

    socket작업완료 및 sampledata json반환

--- routes/sampledataRouter.js
+++ routes/sampledataRouter.js
@@ -3,7 +3,10 @@ const router = express.Router();
 // const db = require("../config/db");
 
 router.get("/sampledata", async (req, res, next) => {
-  res.json({ res: true, testName: "kong", testAge: 24 });
+  res.json(
+    { res: true, testName: "kong", testAge: 24 },
+    { res: false, testName: "lee", testAge: 24 }
+  );
 });
 
 module.exports = router;
