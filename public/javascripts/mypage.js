b8f4fad8874f90cbb0e78a1a400e1cf77ae93692
kongtaemin <rhdxoals@gmail.com>
2021-12-04 22:36:23 +0900

    채팅방 사진

--- public/javascripts/mypage.js
+++ public/javascripts/mypage.js
@@ -58,7 +58,7 @@ socket.on("chat", async (data) => {
           .catch((err) => console.log(err));
         const template = `
         <div class="chat-card" >
-          <div class="chat-photo flex-grow-2"></div>
+          <div class="chat-photo flex-grow-2 style="background-image: url('/${data.sendUserID}.jpg')"></div>
             <div class="chat-description flex-grow-1">
                 <div class="chat-name" data-userid="${data.sendUserID}" data-roomname="${userName}">${userName}</div>
                 <div class="chat-status-msg" id="${data.sendUserID}-last-msg"></div>
